4a1774d9d6b18149594f27e59e2bca91747ba391
LuisaPinillos <luisapinillos20@gmail.com>
2021-07-28 08:08:56 -0500

    add part of the function printf

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _printf -
+ * @format: 
+ * Return: 
+ */
+
+int _printf(const char *format, ...)
+{
+  int count_format = 0;
+  va_list args;
+  va_start (args, format);
+  
+  if (format != NULL && *format != '\0')
+   {
+      while (*format != '\0')
+      {
+        {
+        }
+      }
+   }
+}
