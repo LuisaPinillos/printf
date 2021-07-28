d0d1576ff8e8b137d2fb361b4a64fed91e8e5f18
LauSCaicedo <3383@holbertonschool.com>
2021-07-28 08:45:59 -0500

    Complete the function _printf

--- _printf.c
+++ _printf.c
@@ -2,22 +2,43 @@
 
 /**
  * _printf -
- * @format: 
- * Return: 
+ * @format:
+ * Return:
  */
 
 int _printf(const char *format, ...)
 {
-  int count_format = 0;
-  va_list args;
-  va_start (args, format);
-  
-  if (format != NULL && *format != '\0')
-   {
-      while (*format != '\0')
-      {
-        {
-        }
-      }
-   }
+	int count_format = 0;
+	va_list args;
+	va_start(args, format);
+
+	if (format != NULL && *format != '\0')
+	{
+		while (*format != '\0')
+		{
+			if (*format == '%')
+			{
+				int (*f)(va_list args);
+
+				f = get_op_func(++format);
+				if (f != NULL)
+				{
+					f(args);
+				}
+				else
+				{
+					write(1, format - 1, 2);
+				}
+				format++;
+			}
+			else
+			{
+				write(1, format, 1);
+				count_format++;
+				format++;
+			}
+		}
+	}
+	va_end(args);
+	return (count_format);
 }
