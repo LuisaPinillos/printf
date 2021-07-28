8df4229c67c9254ea9ccfd4ad8b6a6993404b618
LauSCaicedo <3383@holbertonschool.com>
2021-07-28 07:12:49 -0500

    add prototypes and comments

--- holberton.h
+++ holberton.h
@@ -1,16 +1,28 @@
-#ifndef PRINT_FUNCTION_H
-#define PRINT_FUNCTION_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-
+/*LIBRARIES*/
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 
+/*STRUCTURE*/
+/**
+ * struct op - Structure that define the functions.
+ * @op: Pointer.
+ * @f: The function associated.
+ */
 typedef struct op
 {
-    char *op;
-    int (*f)(va_list args);
+	char *op;
+	int (*f)(va_list args);
 } op_t;
+
+/*PROTOTYPES*/
+int _printf(const char *format, ...);
+int op_character(va_list args);
+int op_string(va_list args);
+int op_porcentage(va_list args);
 
 #endif
