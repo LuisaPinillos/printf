3a3c35de1343368fcff10bc977631a5753c77ed3
LuisaPinillos <luisapinillos20@gmail.com>
2021-07-28 18:40:29 -0500

    rename the file lenght spaces function

---
+++ lenght_spaces_func.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * spaces - function that lenght the empty spaces in the argument.
+ * @spc: Argument
+ * Return: number of the spaces
+ */
+
+int spaces(const char *spc)
+{
+    int i = 0;
+
+    while (spc[i] == ' ')
+    {
+    }
+    return (i);
+}
