836e34c72873bb8c24566a56bf9ae0d52658951b
LuisaPinillos <luisapinillos20@gmail.com>
2021-07-28 18:39:21 -0500

    funcion that lenght the empty spaces of the arguments in the printf function

---
+++ count_spaces_func.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * spaces - function that count the spaces in the argument.
+ * @spc: Argument
+ * Return: number of the spaces
+ */
+
+int spaces(const char *spc)
+{
+    int i = 0;
+
+    while (spc[i] == ' ')
+    {
+    }
+    return (i);
+}
