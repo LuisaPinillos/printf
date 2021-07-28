eed19cc79026dd2fb10224e9f49e6b105d553ba0
LauSCaicedo <3383@holbertonschool.com>
2021-07-28 08:42:36 -0500

    complete a function and add other function

--- printf_functions.c
+++ printf_functions.c
@@ -1,32 +1,61 @@
 #include "holberton.h"
 
 /**
- * op_character - Identify the character specifier
- * @args: arguments
- * Return: the character
+ * op_character - Print a character
+ * @args: Arguments.
+ * Return: The character.
  */
 
 int op_character(va_list args)
 {
-  int container = 0; 
-      
-  container = va_arg(args, int);
-  write(1, container, 1);
-  return(1);
+	int container = 0;
+
+	container = va_arg(args, int);
+	write(1, &container, 1);
+	return (1);
 }
 
 /**
- * op_string- Identify the string specifier
- * @args: arguments
- * Return: the string
+ * op_string - Print strings.
+ * @args: Arguments.
+ * Return: The string.
  */
 
 int op_string(va_list args)
 {
-  int i = 0;
-  char *container;
-  
-  container = va_arg(args, char *);
+	int i = 0;
+	char *container;
 
-  return (0);
+	container = va_arg(args, char *);
+	if (container != NULL)
+	{
+		while (container[i] != '\0')
+		{
+			write(1, &container[i], 1);
+			i++;
+		}
+	}
+	else
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	return (i);
+}
+
+/**
+ * op_porcentage - Print the character %.
+ * @args: Arguments.
+ * Return: A character (%).
+ */
+
+int op_porcentage(va_list args)
+{
+	(void)args;
+	if ()
+	{
+	}
+
+	write(1, "%", 1);
+	return (1);
 }
