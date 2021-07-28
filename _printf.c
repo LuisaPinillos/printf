08b7efab7e0f9621b9575faf039cbb48d1b8e0db
LuisaPinillos <luisapinillos20@gmail.com>
2021-07-28 18:28:51 -0500

    complete the function and fix some errors

--- _printf.c
+++ _printf.c
@@ -1,5 +1,16 @@
 #include "holberton.h"
 
+int spaces(const char *spc)
+{
+    int i = 0;
+
+    while (spc[i] == ' ')
+    {
+        i++;
+    }
+    return (i);
+}
+
 /**
  * _printf - Main function of printf.
  * @format: Is a character string.
@@ -8,38 +19,57 @@
 
 int _printf(const char *format, ...)
 {
-	int count_format = 0;
-	va_list args;
+    int (*f)(va_list args);
+    int count_format = 0;
+    va_list args;
 
-	va_start(args, format);
-	if (format != NULL && *format != '\0')
-	{
-		while (*format != '\0')
-		{
-			if (*format == '%')
-			{
-				int (*f)(va_list args);
+    va_start(args, format);
+    if (format != NULL && *format != '\0')
+    {
+        while (*format != '\0')
+        {
+            if (*format == '%')
+            {
+                int acumulator;
+                acumulator = spaces(++format);
+                if (acumulator != 0)
+                {
+                    format += acumulator;
+                    f = get_op_func(format);
+                }
+                else
+                {
+                    f = get_op_func(++format);
+                }
 
-				f = get_op_func(++format);
-				if (f != NULL)
-				{
-					count_format += f(args);
-				}
-				else
-				{
-					write(1, format - 1, 2);
-					count_format += 2;
-				}
-				format++;
-			}
-			else
-			{
-				write(1, format, 1);
-				count_format++;
-				format++;
-			}
-		}
-	}
-	va_end(args);
-	return (count_format);
+                if (f != NULL)
+                {
+                    count_format += f(args);
+                }
+                else
+                {
+                    if (acumulator != 0)
+                    {
+                        write(1, format - (acumulator + 1), 1);
+                        write(1, " ", 1);
+                        write(1, format, 1);
+                    }
+                    else
+                    {
+                        write(1, format - 1, 2);
+                        count_format += 2;
+                    }
+                }
+                format++;
+            }
+            else
+            {
+                write(1, format, 1);
+                count_format++;
+                format++;
+            }
+        }
+    }
+    va_end(args);
+    return (count_format);
 }
