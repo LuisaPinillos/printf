b6a83065c239555a9ac2b46aa1263f0a747506fc
LauSCaicedo <3383@holbertonschool.com>
2021-07-28 20:51:58 -0500

    error

--- _printf.c
+++ _printf.c
@@ -6,6 +6,7 @@
  */
 int _printf(const char *format, ...)
 {
+	int acumulator;
 	int (*f)(va_list args);
 	int count_format = 0;
 	va_list args;
@@ -17,44 +18,31 @@ int _printf(const char *format, ...)
 		{
 			if (*format == '%')
 			{
-				int acumulator;
 				acumulator = spaces(++format);
 				if (acumulator != 0)
 				{
-					format += acumulator;
-					f = get_op_func(format);
+					format += acumulator, f = get_op_func(format);
 				}
 				else
 				{
-					f = get_op_func(++format);
+					f = get_op_func(format);
 				}
-
 				if (f != NULL)
-				{
 					count_format += f(args);
-				}
 				else
 				{
 					if (acumulator != 0)
 					{
 						write(1, format - (acumulator + 1), 1);
-						write(1, " ", 1);
-						write(1, format, 1);
+						write(1, " ", 1), write(1, format, 1);
 					}
 					else
-					{
-						write(1, format - 1, 2);
-						count_format += 2;
-					}
+						write(1, format - 1, 2), count_format += 2;
 				}
 				format++;
 			}
 			else
-			{
-				write(1, format, 1);
-				count_format++;
-				format++;
-			}
+				write(1, format, 1), count_format++, format++;
 		}
 	}
 	va_end(args);
